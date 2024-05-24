3d89b574723b1fb3758baf593e4f779c984adf5a
killin0415 <swordfire1000@gmail.com>
2024-05-24 13:31:50 +0800

    Initialize project using Create React App

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,21 @@
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"
+import timeGridPlugin from "@fullcalendar/timegrid"
+import interactionPlugin from "@fullcalendar/interaction"
+
+export function Calendar() {
+    return (
+        <div>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                initialView={"dayGridMonth"}
+                headerToolbar={{
+                    start: "today prev,next",
+                    center: "title",
+                    end: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+                height={"90vh"}
+            />
+        </div>
+    )
+}
