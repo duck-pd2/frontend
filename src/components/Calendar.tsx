1d0152b268e5ca96ec4233081db5c3893413f86b
shensyuan <sxs940302@gmail.com>
2024-06-05 17:17:12 +0800

    calender events

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,8 +2,26 @@ import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid"
 import timeGridPlugin from "@fullcalendar/timegrid"
 import interactionPlugin from "@fullcalendar/interaction"
+import { useState } from "react";
 
 export function Calendar() {
+
+    const [events, setEvents] = useState([
+        {
+          title: 'Meeting',
+          start: '2024-06-10T10:30:00',
+          end: '2024-06-10T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2024-06-11T12:00:00',
+        },
+        {
+          title: 'Conference',
+          start: '2024-06-13',
+          end: '2024-06-15'
+        }
+      ]);
     return (
         <div>
             <FullCalendar
@@ -15,6 +33,7 @@ export function Calendar() {
                     end: "dayGridMonth,timeGridWeek,timeGridDay",
                 }}
                 height={"90vh"}
+                events={events}
             />
         </div>
     )
